e69af2ec1db11e7c6fcd2ff710546e6d1b1d4bbe
falearq <59762127+falearq@users.noreply.github.com>
2020-10-08 14:14:46 -0500

    cambios en footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,16 @@
+import { findByLabelText } from '@testing-library/react';
 import React from 'react';
+
+import {ReactComponent as Google} from '../../images/googleicon.svg'
+import {ReactComponent as Facebook} from '../../images/facebookicon.svg'
+import {ReactComponent as Twitter} from '../../images/twittericon.svg'
+import {ReactComponent as Instagram} from '../../images/instagramicon.svg'
 const Footers = () => {
     return ( 
                 <div className='footer'>
-                    <div className='socialmedia-bar'></div>
-                   <p> © Instituto Panamericano de Investigación para las Relaciones Exitosas e Inteligentes, A.C.</p>
-                   <p>Cuenca 48 Colonia Álamos CP 03400 Benito Juárez, CDMX.</p>
+                    <div className='socialmedia-bar'><a href='https://google.com'><Google/></a><Facebook/><Twitter/><Instagram/></div>
+                   <p className='footer-title'> © Instituto Panamericano de Investigación para las Relaciones Exitosas e Inteligentes, A.C.</p>
+                   <p className='footer-subtitle'>Cuenca 48 Colonia Álamos CP 03400 Benito Juárez, CDMX.</p>
                 </div> 
     );
 }
