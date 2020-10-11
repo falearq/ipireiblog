ac10d2a649098d365d57b7c35ad2c0d5441a6ed5
falearq <59762127+falearq@users.noreply.github.com>
2020-10-10 20:29:40 -0500

    class .contact-container

---
+++ src/components/ContactForm/Form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Form () {
+    return(
+        <form className='contact-container'>
+            <p> uhuhwie</p>
+
+        </form>
+    )
+}
+
+export default Form 
--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -1,10 +1,10 @@
 import React,{Fragment} from 'react'
-
+import Form from '../components/ContactForm/Form'
 
 const Contacto = () => {
     return ( 
         <Fragment>
-
+            <Form/> 
         </Fragment>
      );
 }
